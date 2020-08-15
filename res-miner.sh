23eb5a633792af301c334d77e678721412ee781f
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-16 06:37:17 +0700

    Update res-miner.sh

--- res-miner.sh
+++ res-miner.sh
@@ -1,1 +1,1 @@
-./resminer-sse2 -a yespowerRES -o stratum+tcp://sea.mine.zpool.ca:6233 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 8
+./resminer-sse2 -a yespowerRES -o stratum+tcp://sea.mine.zpool.ca:6234 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 8
